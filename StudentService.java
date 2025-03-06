dae791679ec75b740eee52414dae715196498014
Abhijeet Bagade <bagadeabhijeet408@gmail.com>
2025-03-06 13:32:26 +0530

    Add files via upload

---
+++ StudentService.java
@@ -0,0 +1,17 @@
+package in.springbootfirstproject.service;
+
+import java.util.List;
+
+import in.springbootfirstproject.entity.Student;
+
+public interface StudentService {
+	
+	public List<Student> getAllStudents();
+	
+	public Student saveStudent(Student student);
+	
+	public Student getById(int id);
+	
+	public void deleteById(int id);
+
+}
